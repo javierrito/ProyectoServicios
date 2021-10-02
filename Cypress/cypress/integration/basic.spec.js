b917d823c91d36e58efa5db635353311bd549393
javi2 <javier.rito.hernandez@gmail.com>
2021-10-02 08:53:43 -0500

    Se modifica la ip cypress

--- Cypress/cypress/integration/basic.spec.js
+++ Cypress/cypress/integration/basic.spec.js
@@ -1,7 +1,7 @@
 describe('Prueba local', () => {
 
     it('Test', () => {
-        cy.visit('http://172.24.80.1:8010/')
+        cy.visit('http://192.168.100.13:8010/')
         cy.get('#mat-input-0').clear().type('admin')
         cy.get('.mat-raised-button').click()
         cy.get('h1').should('contain', 'About')
